96f63049cbbde3cc8e33e5db61129ee8fe9971e6
karantec <karanrana3095@gmail.com>
2025-01-20 12:33:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,39 +1,11 @@
-import React, { useState } from "react";
-import { Route, Routes, useLocation } from "react-router-dom";
-import Navbar from "./Component/Navbar";
-import CartSidebar from "./Component/CartSidebar";
-import Home from "./Component/Home";
-import OrderCheckout from "./Component/OrderCheckout";
-import TrackOrder from "./Component/TrackOrder";
-import InventoryManagement from "./Component/InventoryManagement";
-import ReturnOrder from "./Component/ReturnOrder";
+import Home from "./Components/Home"
 
 const App = () => {
-  const [isCartOpen, setIsCartOpen] = useState(false);
-  const location = useLocation();
-
-  // Update showNavbar condition to hide navbar on return-order route as well
-  const showNavbar = location.pathname === '/'
   return (
     <div>
-      {showNavbar && (
-        <>
-          <Navbar onCartClick={() => setIsCartOpen(true)} />
-          <CartSidebar
-            isOpen={isCartOpen}
-            onClose={() => setIsCartOpen(false)}
-          />
-        </>
-      )}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/checkout" element={<OrderCheckout />} />
-        <Route path="/track-order" element={<TrackOrder />} />
-        <Route path="/inventory" element={<InventoryManagement />} />
-        <Route path="/return" element={<ReturnOrder />} /> 
-      </Routes>
+       <Home/>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
